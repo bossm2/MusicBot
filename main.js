99d5561deaa1fff46cb0ad18e8bb64685026110a
bossm2 <im_one_man@yahoo.com>
2019-08-04 10:40:35 -0700

    h

---
+++ main.js
@@ -0,0 +1,57 @@
+//#region variables
+//fff
+var request = require('requestretry');
+var EventSource = require("eventsource");
+var cheerio = require('cheerio');
+//const puppeteer = require('puppeteer');
+function myRetryStrategy(err, response, body, options) {
+    //console.log(err);
+	return (typeof body == 'undefined' || (err != null));
+}
+
+//#endregion
+
+turlest=encodeURI("http://next1.ir/?s=شادمهر");
+// browser =  puppeteer.launch();
+// page = browser.newPage();
+// setInterval(function(){
+
+// (async () => {
+    
+    
+        
+//         await page.goto(turlest, {waitUntil: 'load'});
+
+//         const newPage = await page.evaluate(() => {
+//         return  document.getElementsByClassName('ft-more')[0].outerText
+//         });
+        
+//     console.log(newPage)
+
+// })()
+// },9000)
+
+// console.log('newPage')
+
+// //console.log(turlest)
+setTimeout( function (i) {
+    request({
+        url: (turlest),
+        method: "GET",
+        maxAttempts: 2,
+        retryDelay: 100,
+        retryStrategy: myRetryStrategy},
+        function (error, response, body) {
+            //console.log(body);
+	if(error) {
+    console.log("Error: " + error);
+    }
+    else{
+            $ = cheerio.load(body)
+			title1 = $('strong','span').children('.span')//.attr('ft-more')
+			//title2 = $(title1).text().trim();
+			};
+    console.log(title1);
+    });
+
+});
---
+++ main2.js
@@ -0,0 +1,57 @@
+//#region
+const cheerio = require('cheerio')
+const $ = cheerio.load(`<!DOCTYPE html>
+<html>
+<head>
+<style>
+ul {
+list-style-type: none;
+margin: 0;
+padding: 0;
+overflow: hidden;
+background-color: #333333;
+}
+
+li {
+float: left;
+}
+
+li a {
+display: block;
+color: white;
+text-align: center;
+padding: 16px;
+text-decoration: none;
+}
+
+li a:hover {
+background-color: #111111;
+}
+</style>
+</head>
+<body>
+
+<ul>
+<li><a href="#home">Home</a></li>
+<li><a href="#news">News</a></li>
+<li class="aa" id="aaa"><a href="#contact">Contact</a></li>
+<li><a href="#about">About</a></li>
+</ul>
+
+</body>
+</html>`)
+//#endregion
+
+
+// $('h2.title').text('Hello there!')
+// $('h2').addClass('welcome')
+// $('.title').text()
+// console.log($.html());
+// console.log($('#aaa').eq(0).text());
+// console.log($('li').children().get(0).tagName);
+// console.log($('li').html().get(0).text());
+var hobbies = [];
+$('li').each(function (i, e) {
+    hobbies[i] = $(this).text();
+    console.log(hobbies[i]);
+});
